d3ab599c45993785ff0973b5a620d7375b8db08d
Corey Good <corey.good@sendgrid.com>
2013-08-27 15:55:15 -0600

    trying without attachment

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,9 +2,9 @@ class EmailProcessor
 	def self.process(email)
 		attach = email.attachments
 		if attach.empty?
-			Post.create!({ body: email.body, email: email.from, attachment: "No data" })
+			Post.create!({ body: email.body, email: email.from })#, attachment: "No data" })
 		else
-			Post.create!({ body: email.body, email: email.from, attachment: attach[0] })
+			Post.create!({ body: email.body, email: email.from })#, attachment: attach[0] })
 		end
 	end
 end
