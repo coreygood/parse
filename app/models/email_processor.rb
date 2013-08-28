9392e9d83fecb6b4c7fbeb10e801a45848e3eeb0
Corey Good <corey.good@sendgrid.com>
2013-08-27 21:35:33 -0600

    Add code to output the call information values from the csv file for the current day; testing on heroku to see if the file will be stored locally

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,10 +1,16 @@
+require 'csv'
 class EmailProcessor
 	def self.process(email)
 		attach = email.attachments
+		puts "attach: #{attach}\n"#attach.tempfile: #{attach.tempfile}"
+		puts "Latest Row: #{CSV.read(attach[0].tempfile)[7]}"
+		puts "Latest Row Calls Only: #{CSV.read(attach[0].tempfile)[7][2]}"
+		#binding.pry
 		if attach.empty?
-			Post.create!({ body: email.body, email: email.from })#, attachment: "No data" })
+			Post.create!({ body: email.body, email: email.from, attachment: "No data" })
 		else
-			Post.create!({ body: email.body, email: email.from })#, attachment: attach[0] })
+#			CSV.foreach()
+			Post.create!({ body: email.body, email: email.from})#, attachment: attach[0] })
 		end
 	end
 end
